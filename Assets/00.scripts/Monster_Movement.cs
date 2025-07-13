aed094197764cc5dc678e74f2bc248441e6d25c5
rnjsxogus0705 <rnjsxogus0705@gmail.com>
2025-07-13 15:37:39 +0900

    챕터 5 (중) 완료
    몬스터들이 플레이어를 바라보며 추적하도록 코딩함(소한도 자연스러워짐)

--- Assets/00.scripts/Monster_Movement.cs
+++ Assets/00.scripts/Monster_Movement.cs
@@ -1,3 +1,4 @@
+    using System.Collections;
     using UnityEngine;
 
     public class Monster_Movement : MonoBehaviour
@@ -5,22 +6,53 @@
         public Transform target;
         public float speed = 3.0f;
         private Rigidbody rb;
-
+        bool ispanwed = false;
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
         }
-        public void SetTarget(Transform player)
+        
+        public void Initalize(Transform player)
         {
             target = player;
+            StartCoroutine(SpawnStartCoroutine(transform.localScale));
+        }
+
+        IEnumerator SpawnStartCoroutine(Vector3 scaleEnd)
+        {
+            Vector3 ScaleStart = Vector3.zero;
+            Vector3 ScaleEnd = scaleEnd;
+            float duration = 0.5f;
+            float timer = 0.0f;
+            while (timer < duration)
+            {
+                float t = timer / duration;
+                transform.localScale = Vector3.Lerp(ScaleStart, ScaleEnd, t);
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            
+            ispanwed = true;
         }
 
         private void FixedUpdate()
         {
-            if (target != null)
-            {
-                Vector3 direction = (target.position - transform.position).normalized;
-                rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
-            }
+            if (!ispanwed) return;
+            
+            MoveAndRotate();
         }
-    }
+
+        void MoveAndRotate()
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            direction.y = 0f;
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+            }
+            
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        } 
+}
