6bbfd5357f351b5b53d08a0ec1914bce28193fe3
rnjsxogus0705 <rnjsxogus0705@gmail.com>
2025-07-13 21:02:27 +0900

    챕터8 완료
    데미지가 연출되었다

--- Assets/00.scripts/Bullet.cs
+++ Assets/00.scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -5,6 +6,7 @@ public class Bullet : MonoBehaviour
     public float speed = 10.0f;
     public float lifetime = 5.0f;
     public GameObject ExplosionParticle;
+    public GameObject DamageObject;
     private Vector3 direction;
     
 
@@ -24,6 +26,11 @@ public class Bullet : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Monster"))
         {
             Instantiate(ExplosionParticle, transform.position, Quaternion.identity);
+            var damageFont = Instantiate(DamageObject);
+                 damageFont.GetComponent<DamageTMP>().Initalize(
+                 Base_Canvas.instance.transform,
+                 transform.position,
+                 "10");
             Destroy(this.gameObject);
         }
     }
